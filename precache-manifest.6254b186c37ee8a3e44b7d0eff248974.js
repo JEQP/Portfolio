d6f5ee3b6c8696144da7f5b94b5287ff263d3494
James Pearce <jamesquintanapearce@gmail.com>
2020-04-14 12:44:23 +1000

    Updates

---
+++ precache-manifest.6254b186c37ee8a3e44b7d0eff248974.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9160c7420aed8217c304b28f2fba0142",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b3b6e53fff676f22a862",
+    "url": "/portfolio/static/css/main.d6cfed08.chunk.css"
+  },
+  {
+    "revision": "bf8e3ce0dc199ebe2673",
+    "url": "/portfolio/static/js/2.1f1f9999.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.1f1f9999.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3b6e53fff676f22a862",
+    "url": "/portfolio/static/js/main.881ebea8.chunk.js"
+  },
+  {
+    "revision": "65f5e142e492e2a0942e",
+    "url": "/portfolio/static/js/runtime-main.80e88482.js"
+  }
+]);
