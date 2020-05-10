59a4c7c7caef35e989db97cc8236153e8fabe6c7
James Pearce <jamesquintanapearce@gmail.com>
2020-05-11 00:09:39 +1000

    build

---
+++ build/precache-manifest.03c81f4d19fb15f4543edf768b110229.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fbc820dd52203256fbc51a412948eb9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "25362e2f6b80d677f6c8",
+    "url": "/portfolio/static/css/main.8060db7b.chunk.css"
+  },
+  {
+    "revision": "fb6ac2c69b53ec6b9c31",
+    "url": "/portfolio/static/js/2.f619239e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.f619239e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25362e2f6b80d677f6c8",
+    "url": "/portfolio/static/js/main.a0cc966e.chunk.js"
+  },
+  {
+    "revision": "65f5e142e492e2a0942e",
+    "url": "/portfolio/static/js/runtime-main.80e88482.js"
+  },
+  {
+    "revision": "05741b89485d445f846b655d7e3e56c7",
+    "url": "/portfolio/static/media/Profile1.05741b89.jpg"
+  },
+  {
+    "revision": "42f3515312c6bc55a687e4eb017916a5",
+    "url": "/portfolio/static/media/background.42f35153.jpg"
+  }
+]);
